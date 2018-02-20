20c8f43edaa1579cb05d3370e0be1ad7a2bc7f28
o-lat <omarl1933@gmail.com>
2018-02-20 10:18:09 +0000

    improved Aurum suggestions

--- aurum/tags.js
+++ aurum/tags.js
@@ -1,30 +1,17 @@
 // JavaScript Document
 
 $(function() {
-			var tags = [
-			  "are you male or female",
-			  "who made you",
-			  "who are you",
-			  "who is your creator",
-			  "what is the time",
-			  "what is your age",
-			  "what is your name",
-			  "what gender are you",
-			  "what is the weather like",
-			  "okay assistant",
-			  "about me",
-			  "http://",
-			  "https://",
-			  "how are you",
-			  "how old are you",
-			  "show me the time",
-			  "show me commands",
-			  "show me the weather",
-			  "tell me a joke"
-			];
-			$( "#srch" ).autocomplete({
-			   minLength:2,   
-			   delay:0,   
-			   source: tags
-			});
-		 });
+    var tags = [
+        "who are you",
+        "what is the time",
+        "what is the weather like",
+        "http://",
+        "https://",
+        "tell me a joke"
+    ];
+    $( "#srch" ).autocomplete({
+        minLength:3,
+        delay:0,
+        source: tags
+    });
+});
