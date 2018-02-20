215961f2474586d9034badded7b5cf740a5b87d0
o-lat <omarl1933@gmail.com>
2018-02-20 17:04:18 +0000

    bug fix

--- new/assets/script.js
+++ new/assets/script.js
@@ -12,18 +12,19 @@ $(document).ready(function(){
     } else {
         $.cookie('old-layout','0');
     }
+    $('#toggle_layout').click(function(){
+        console.log('click');
+        if ($.cookie('old-layout') == '0'){
+            $.cookie('old-layout', '1');
+            $(document.body).removeClass('home');
+            $('.wrap').css('display','block');
+            $('.footer').html('<p>If you prefer the new layout, you can <a href="#" id="toggle_layout" class="link">upgrade</a></p>');
+        } else if ($.cookie('old-layout') == '1'){
+            $.cookie('old-layout', '0');
+            $(document.body).addClass('home');
+            $('.wrap').css('display','none');
+            $('.footer').html('<p>If you prefer the old layout, you can <a href="#" id="toggle_layout" class="link">revert</a></p>');
+        }
+    });
 });
 
-$('#toggle_layout').click(function(){
-    if ($.cookie('old-layout') == '0'){
-        $.cookie('old-layout', '1');
-        $(document.body).removeClass('home');
-        $('.wrap').css('display','block');
-        $('.footer').html('<p>If you prefer the new layout, you can <a href="#" id="toggle_layout" class="link">upgrade</a></p>');
-    } else if ($.cookie('old-layout') == '1'){
-        $.cookie('old-layout', '0');
-        $(document.body).addClass('home');
-        $('.wrap').css('display','none');
-        $('.footer').html('<p>If you prefer the old layout, you can <a href="#" id="toggle_layout" class="link">revert</a></p>');
-    }
-});
