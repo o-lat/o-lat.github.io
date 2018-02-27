a90da41c673f27f0e86a0060fd1e6a8a52ce492b
o-lat <omarl1933@gmail.com>
2018-02-27 16:39:12 +0000

    bug fix

--- aurum/tags.js
+++ aurum/tags.js
@@ -4,7 +4,7 @@ $(function() {
     var tags = [];
     $('#srch').keypress(function(e){
         if($('#srch').val().length > 1){
-            $.getJSON('http://api.bing.com/osjson.aspx?JsonType=callback&JsonCallback=?', {
+            $.getJSON('https://api.bing.com/osjson.aspx?JsonType=callback&JsonCallback=?', {
                 query: $('#srch').val()
             }, function(data) {
                 for(i = 0; i < data[1].length; i++){
@@ -23,32 +23,3 @@ $(function() {
     });
     
 });
-
-/*$.ajax({
-                type: 'GET',
-                dataType: 'xml',
-                url: 'http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=' + $('#srch').val(),
-                success: function(data){
-                    $xml = $(data),
-                    $suggestion = $xml.find("suggestion");
-                    for(i = 0; i < $suggestion.length; i++){
-                        var suggestion = $suggestion[i].attributes[0].nodeValue;
-                        tags.push(suggestion);
-                        $('#srch').autocomplete({
-                            minLength:1,
-                            delay:0,
-                            source: tags
-                        });
-                    }
-                    tags = [];
-                },
-                error: function(xhr,error,status){
-                    tags = 'Error while getting Google suggestions ' + xhr.status;
-                    console.log(tags)
-                    $('#srch').autocomplete({
-                        minLength:1,
-                        delay:0,
-                        source: tags
-                    });
-                }
-            });*/
