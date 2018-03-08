96136d899239022bd5ed76cad03f680046d3d908
o-lat <omarl1933@gmail.com>
2018-03-08 13:34:14 +0000

    Using own cors server

--- aurum/script.js
+++ aurum/script.js
@@ -318,7 +318,7 @@ function searchResults() {
             console.log('Error: ' + xhr.status);
         }
     });*/
-    var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+    var cors_api_url = 'https://o-lat.herokuapp.com/';
     function doCORSRequest(options, printResult) {
         var x = new XMLHttpRequest();
         x.open(options.method, cors_api_url + options.url);
